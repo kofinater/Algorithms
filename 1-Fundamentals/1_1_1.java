b2b795dac5fde9e1fe063587bd7e642a72b14557
kofinater <oseikk3@gmail.com>
2018-01-01 16:03:08 -0500

    1.1.1 Java
    Code to verify answers to question 1.1.1

---
+++ 1-Fundamentals/1_1_1.java
@@ -0,0 +1,7 @@
+public class 1_1_1{
+	public static void main(String[] args){
+		System.out.println((0+15)/2); //Problem 'a'
+		System.out.println(2.0e-6 * 100000000.1);//Problem 'b'
+		Systen.out.println(true && || true && true);//Problem 'c'
+	}
+}
